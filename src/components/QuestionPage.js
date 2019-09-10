8aa0881fc395d36bdb7f44b259c92faad48556ee
PriyaRSurendranath <priyami2010@gmail.com>
2019-09-10 15:17:54 -0400

    Checked answer and display the marks

--- src/components/QuestionPage.js
+++ src/components/QuestionPage.js
@@ -21,13 +21,16 @@ class QuestionPage extends React.Component {
 
     handleSubmit(event) {
     alert('A name was submitted: ' + this.state.value);
-    if(this.state.value == this.props.question.answer){
-        this.state.isCorrect = true;
-        this.state.answered =true;
+    if(this.state.value === this.props.question.answer){
+        this.setState({isCorrect: !this.state.isCorrect});
+        this.setState({display: !this.state.display});
         alert("The answer is correct");
+        console.log(this.state.isCorrect);
+        console.log(this.state.display);
     }
     else{
         alert("The answer is wrong");
+         this.setState({display: this.state.display});
     }
     event.preventDefault();
     }
@@ -37,14 +40,24 @@ class QuestionPage extends React.Component {
         const question = this.props.question ? this.props.question.question : "loading ...";
         console.log(question);
         const step = this.props.question ? this.props.question.step : "loading ...";
+        const classNameBtn = this.state.isCorrect ? 'button-right' : 'button-wrong';
+        console.log(classNameBtn);
+        const classNameToggle = !this.state.display ? 'button-add' : classNameBtn;
         
+        console.log(classNameToggle);
+        
+
+        
+
         return <div> {question}
                 <br />
                      {/* {step}  */}
 
                  <form onSubmit={this.handleSubmit}>
-                    <label> <input type="text" value={this.state.value} onChange={this.handleChange} className = "text-add" /> </label>
-                    <input type="submit" value="" className = "button-add" />
+                    <label> <input type="text" value={this.state.value} onChange={this.handleChange} className = "text-add"/> </label>
+                    
+                   <input type= "submit"  value=""  className = {classNameToggle}/>
+                   
                 </form>
                </div>
 
